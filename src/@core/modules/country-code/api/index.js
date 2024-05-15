ef686d1541516e8c80b5f0dff48051ccbe9eb3d6
ming <mick66145@gmail.com>
2024-05-15 10:07:16 +0800

    refactor: modules country-code

---
+++ src/@core/modules/country-code/api/index.js
@@ -0,0 +1,2 @@
+
+export { default as CountryCodeResource } from './restful/country-code'
--- src/api/restful/modules/basic-base/index.js
+++ src/api/restful/modules/basic-base/index.js
@@ -1,13 +1,11 @@
 import auth from './modules/auth'
 import file from './modules/file'
 import menuPermission from './modules/menu-permission'
-import countryCode from './modules/country-code'
 
 export const exportBaseApiModules = () => {
   const modules = {}
   modules.AuthResource = auth
   modules.FileResource = file
   modules.MenuPermissionResource = menuPermission
-  modules.CountryCodeResource = countryCode
   return modules
 }
