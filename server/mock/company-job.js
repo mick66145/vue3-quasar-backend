d9be6ba6ac3416845f2fc0d122287199439d21f7
ming <mick66145@gmail.com>
2024-05-25 23:03:28 +0800

    refactor(mock): company-job

--- server/mock/company-job.js
+++ server/mock/company-job.js
@@ -1,4 +1,4 @@
-import { searchKeysValue } from './utils/filter'
+import Controller from './http/controller'
 
 const companyJobList = [
   {
@@ -8,91 +8,14 @@ const companyJobList = [
     sequence: 0,
   },
 ]
-let id = 2
+const keywordColumn = ['name']
+const companyController = new Controller('api/company_job')
+companyController.setList(companyJobList)
 
 export default [
-  {
-    url: '/api/company_job',
-    method: 'get',
-    response: config => {
-      // eslint-disable-next-line camelcase
-      const { keyword, page = 1, page_size = 20 } = config.query
-      const keywordFilter = {
-        name: keyword,
-        tel: keyword,
-        tel_service: keyword,
-        address: keyword,
-        email: keyword,
-      }
-      const mockList = keyword ? searchKeysValue([...companyJobList], keywordFilter) : [...companyJobList]
-      // eslint-disable-next-line camelcase
-      const pageList = mockList.filter((item, index) => index < page_size * page && index >= page_size * (page - 1))
-      return {
-        code: 20000,
-        data: {
-          list: pageList,
-          meta: {
-            pagination: {
-              total: mockList.length,
-              // eslint-disable-next-line camelcase
-              count: page_size,
-            },
-          },
-        },
-      }
-    },
-  },
-  {
-    url: '/api/company_job/:id(\\d+)',
-    method: 'get',
-    response: config => {
-      const { id } = config.query
-      for (const companyJob of companyJobList) {
-        if (+companyJob.id === +id) {
-          return {
-            code: 20000,
-            data: companyJob,
-          }
-        }
-      }
-    },
-  },
-  {
-    url: '/api/company_job',
-    method: 'post',
-    response: options => {
-      const body = options.body
-      body.id = id
-      id += 1
-      companyJobList.push(body)
-      return {
-        code: 20000,
-        data: body,
-      }
-    },
-  },
-  {
-    url: '/api/company_job/:id(\\d+)',
-    method: 'patch',
-    response: config => {
-      const { id } = config.query
-      const index = companyJobList.findIndex(x => x.id === id)
-      return {
-        code: 20000,
-        data: 'success',
-      }
-    },
-  },
-  {
-    url: '/api/company_job/:id(\\d+)',
-    method: 'delete',
-    response: config => {
-      const { id } = config.query
-      const index = companyJobList.findIndex(x => x.id === id)
-      return {
-        code: 20000,
-        data: 'success',
-      }
-    },
-  },
+  companyController.show(),
+  companyController.index({keywordColumn}),
+  companyController.store(),
+  companyController.update(),
+  companyController.delete(),
 ]
