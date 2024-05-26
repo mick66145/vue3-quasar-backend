696ea0278846e65fcbdbb9430fa45c82c2adda00
ming <mick66145@gmail.com>
2024-05-26 19:08:33 +0800

    feat(ListItem1): 判斷沒有icon字體就置中

--- src/@core/components/list-item/ListItem1.vue
+++ src/@core/components/list-item/ListItem1.vue
@@ -1,30 +1,27 @@
 <template>
-  <div>
-    <q-card
-      class="border-primary"
-      :class="active && 'active'"
-      flat
-      bordered
-    >
-      <q-item class="px-7 q-my-lg">
-        <q-item-section avatar>
-          <q-icon :name="icon" :color="active?'white':'primary'" size="1.7rem" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label class="text-base">{{ title }}</q-item-label>
-          <q-item-label caption lines="1">{{ subtitle }}</q-item-label>
-        </q-item-section>
+  <q-card class="border-primary cursor-pointer" :class="active && 'active'" flat bordered>
+    <q-item class="px-7 q-my-lg">
+      <q-item-section avatar v-if="icon">
+        <q-icon
+          :name="icon"
+          :color="active ? 'white' : 'primary'"
+          size="1.7rem"
+        />
+      </q-item-section>
+      <q-item-section>
+        <q-item-label class="text-base" :class="!icon && 'text-center'">{{title}}</q-item-label>
+        <q-item-label v-if="subtitle" caption lines="1" :class="!icon && 'text-center'">{{ subtitle }}</q-item-label>
+      </q-item-section>
 
-        <q-item-section v-if="$slots.side" side>
-          <slot name="side" />
-        </q-item-section>
-      </q-item>
-    </q-card>
-  </div>
+      <q-item-section v-if="$slots.side" side>
+        <slot name="side" />
+      </q-item-section>
+    </q-item>
+  </q-card>
 </template>
 
 <script>
-import { defineComponent } from 'vue-demi'
+import { defineComponent } from "vue-demi";
 
 export default defineComponent({
   props: {
@@ -33,26 +30,23 @@ export default defineComponent({
     subtitle: { type: String },
     active: { type: Boolean, default: false },
   },
-  setup () {
-    return {
-    }
+  setup() {
+    return {};
   },
-})
+});
 </script>
 
 <style lang="postcss" scoped>
-:deep() {
-  &.q-card {
-    &.active {
-      @apply bg-primary;
+.q-card {
+  &.active {
+    @apply bg-primary;
 
-      .q-item__label {
-        @apply text-white;
-      }
+    .q-item__label {
+      @apply text-white;
+    }
 
-      .q-btn {
-        background: white !important;
-      }
+    .q-btn {
+      background: white !important;
     }
   }
 }
