363d6f87bdad833caabc1220e936c7f9eac2200d
ming <mick66145@gmail.com>
2024-05-27 11:53:29 +0800

    refactor(store): import api

--- src/stores/platformAttribute.js
+++ src/stores/platformAttribute.js
@@ -1,14 +1,20 @@
-import { PlatformAttributeResource } from '@/@core/modules/platform-attribute/api'
 import { defineStore } from 'pinia'
 
 export const usePlatformAttribute = defineStore({
   id: 'platformAttribute',
   state: () => ({
-    platformAttributeResource: new PlatformAttributeResource(),
+    platformAttributeResource: null,
     platformAttribute: '',
   }),
   actions: {
-    getPlatformAttribute () {
+    async initResources() {
+      const { PlatformAttributeResource } = await import('@/@core/modules/platform-attribute/api');
+      this.platformAttributeResource = new PlatformAttributeResource();
+    },
+    async getPlatformAttribute () {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       return new Promise((resolve, reject) => {
         this.platformAttributeResource.get()
           .then(res => {
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,5 +1,3 @@
-import { UserResource } from '@/@core/modules/user/api'
-import { AuthResource } from '@/@core/modules/auth/api'
 import { getToken, setToken, removeToken } from '@/@core/utils/auth'
 import { defineStore } from 'pinia'
 import useLogout from '@/hooks/useLogout'
@@ -10,12 +8,20 @@ export const useUser = defineStore({
     token: getToken(),
     info: '',
     permissionList: [],
-    userResource: new UserResource(),
-    authResource: new AuthResource(),
+    userResource: null,
+    authResource: null,
   }),
   actions: {
-
-    login (payload) {
+    async initResources() {
+      const { UserResource } = await import('@/@core/modules/user/api')
+      const { AuthResource } = await import('@/@core/modules/auth/api')
+      this.userResource = new UserResource()
+      this.authResource = new AuthResource()
+    },
+    async login (payload) {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       return this.authResource.login(payload)
         .then(res => {
           const { data } = res
@@ -23,7 +29,10 @@ export const useUser = defineStore({
           return res
         })
     },
-    register (payload) {
+    async register (payload) {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       /* 如果有註冊的api就使用以下註解 */
       // return this.authResource.register(payload)
       //   .then(res => {
@@ -33,7 +42,10 @@ export const useUser = defineStore({
         resolve(true)
       })
     },
-    forgetPassword (payload) {
+    async forgetPassword (payload) {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       /* 如果有註冊的api就使用以下註解 */
       // return this.authResource.forgetPassword(payload)
       //   .then(res => {
@@ -44,7 +56,10 @@ export const useUser = defineStore({
       })
     },
 
-    getVerifyCode (payload) {
+    async getVerifyCode (payload) {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       /* 如果有註冊的api就使用以下註解 */
       // return this.authResource.getVerifyCode(payload)
       //   .then(res => {
@@ -55,7 +70,10 @@ export const useUser = defineStore({
       })
     },
 
-    whoami () {
+    async whoami () {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       return new Promise((resolve, reject) => {
         this.authResource.me()
           .then(res => {
@@ -71,7 +89,10 @@ export const useUser = defineStore({
       })
     },
 
-    permission () {
+    async permission () {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       return new Promise((resolve, reject) => {
         this.authResource.permission()
           .then(res => {
@@ -84,11 +105,17 @@ export const useUser = defineStore({
       })
     },
 
-    profile (payload) {
+    async profile (payload) {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       return this.authResource.profile(payload)
     },
 
-    changePassword (payload) {
+    async changePassword (payload) {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       return new Promise((resolve, reject) => {
         return this.authResource.changePassword(payload).then(res => {
           const { resetStore } = useLogout()
@@ -101,7 +128,10 @@ export const useUser = defineStore({
       })
     },
 
-    logout () {
+    async logout () {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       /* 如果有登出的api就使用以下註解 */
       // return new Promise((resolve, reject) => {
       //   return this.authResource.logout().then(res => {
@@ -115,7 +145,10 @@ export const useUser = defineStore({
       this.clear()
     },
 
-    refreshToken (payload) {
+    async refreshToken (payload) {
+      if (!this.authResource) {
+        await this.initResources()
+      }
       return this.authResource.refreshToken(payload)
         .then(res => {
           const { data } = res
